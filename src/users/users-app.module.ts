bad0e028ae375df8f00f4ccbd550510e452fdbd5
Duy Nguyen <duy.nguyenanh01@gmail.com>
2025-01-02 21:51:20 +0700

    add route users/products

--- src/users/users-app.module.ts
+++ src/users/users-app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { RouterModule } from '@nestjs/core';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
   imports: [
     AuthModule,
+    ProductsModule,
     RouterModule.register([
       {
         path: 'users',
         module: AuthModule,
+      },
+      {
+        path: 'users',
+        module: ProductsModule,
       },
     ]),
   ],
