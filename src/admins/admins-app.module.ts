549ec6251aa04e3c7f3bdef27a12dbdf080cb3cc
Duy Nguyen <duy.nguyenanh01@gmail.com>
2025-01-04 16:28:53 +0700

    deploy

--- src/admins/admins-app.module.ts
+++ src/admins/admins-app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AdminsModule } from './admins/admins.module';
 import { AuthModule } from './auth/auth.module';
 import { RouterModule } from '@nestjs/core';
-// import { ProductsModule } from 'src/products/products.module';
 import { CategoriesModule } from 'src/categories/categories.module';
 import { CodesModule } from 'src/codes/codes.module';
 import { UploadsModule } from 'src/uploads/uploads.module';
@@ -12,7 +11,6 @@ import { UsersModule } from 'src/users/users/users.module';
   imports: [
     AdminsModule,
     AuthModule,
-    // ProductsModule,
     CategoriesModule,
     CodesModule,
     UploadsModule,
@@ -26,10 +24,6 @@ import { UsersModule } from 'src/users/users/users.module';
         path: 'admins',
         module: AuthModule,
       },
-      // {
-      //   path: 'admins',
-      //   module: ProductsModule,
-      // },
       {
         path: 'admins',
         module: CategoriesModule,
