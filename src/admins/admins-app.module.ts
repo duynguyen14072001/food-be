62b57b4d05391acd854198f573a9ee42efc12edb
Duy Nguyen <duy.nguyenanh01@gmail.com>
2025-01-04 20:18:15 +0700

    fixbug import, deploy vercel

--- src/admins/admins-app.module.ts
+++ src/admins/admins-app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminsModule } from './admins/admins.module';
 import { AuthModule } from './auth/auth.module';
 import { RouterModule } from '@nestjs/core';
-import { CategoriesModule } from 'src/categories/categories.module';
-import { CodesModule } from 'src/codes/codes.module';
-import { UploadsModule } from 'src/uploads/uploads.module';
-import { UsersModule } from 'src/users/users/users.module';
+import { CategoriesModule } from '../categories/categories.module';
+import { CodesModule } from '../codes/codes.module';
+import { UploadsModule } from '../uploads/uploads.module';
+import { UsersModule } from '../users/users/users.module';
 
 @Module({
   imports: [
